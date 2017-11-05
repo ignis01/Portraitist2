3f29feb0e3feadd1b17f2f156188480991b1c011
Kris Chen <email@krischen.ca>
2017-11-05 18:12:09 -0500

    change to update aws cognito credential with aws id + aws cognito unauthenticated role in order to get pass the cognito unauthenticated access to S3

--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {IonicPage, NavController, NavParams, Slides} from 'ionic-angular';
 import {OnlineOrder} from "../../model/onlineorder";
 import * as AWS from 'aws-sdk';
+
 /**
  * Generated class for the OrderPage page.
  *
@@ -18,33 +19,36 @@ import * as AWS from 'aws-sdk';
 
 export class OrderPage {
   @ViewChild(Slides) slides: Slides;
-  nextBtnLabel:String;
-  backBtnLabel:String;
-  onlineOrder:OnlineOrder;
+  nextBtnLabel: String;
+  backBtnLabel: String;
+  onlineOrder: OnlineOrder;
   backHidden: boolean;
   bucketName = 'portraitist-customer-upload';
   bucketRegion = 'us-east-1';
   identityPoolId = 'us-east-1:5ce684af-cc60-437d-980b-6039d7bbc851';
-  s3:AWS.S3;
+  s3: AWS.S3;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.nextBtnLabel="Next";
-    this.backBtnLabel="Back";
+    this.nextBtnLabel = "Next";
+    this.backBtnLabel = "Back";
     this.backHidden = true;
     this.onlineOrder = new OnlineOrder();
     let creds = new AWS.CognitoIdentityCredentials({
-                IdentityPoolId:'us-east-1:5ce684af-cc60-437d-980b-6039d7bbc851'
-         });
+      AccountId: '000000000000', // replace your AWS account ID here
+      RoleArn: 'arn:aws:iam::000000000000:role/Cognito_your_Unauthenticated_Role', //replace your cognito unauthenticated role here
+      IdentityPoolId: this.identityPoolId
+    });
 
-         AWS.config.update({
-                region:'us-east-1',
-                credentials:creds
-         });
+    AWS.config.update({
+      region: 'us-east-1',
+      credentials: creds
+    });
+
     this.s3 = new AWS.S3({
-     apiVersion: '2006-03-01',
-     params:{Bucket: 'portraitist-customer-upload'}
-   })
-}
+      apiVersion: '2006-03-01',
+      params: {Bucket: 'portraitist-customer-upload'}
+    })
+  }
 
 
   ionViewDidLoad() {
@@ -55,55 +59,55 @@ export class OrderPage {
 
   }
 
-  next(){
+  next() {
     this.slides.lockSwipes(false);
     this.slides.slideNext(500)
-    if(this.slides.getActiveIndex() == this.slides.length()-1){
-       this.nextBtnLabel="Finish"
+    if (this.slides.getActiveIndex() == this.slides.length() - 1) {
+      this.nextBtnLabel = "Finish"
     }
     this.backHidden = false;
     this.slides.lockSwipes(true);
   }
 
-  prev(){
+  prev() {
     this.slides.lockSwipes(false);
     this.slides.slidePrev(500)
-    if(this.slides.getActiveIndex() < this.slides.length()-1){
-           this.nextBtnLabel="Next"
+    if (this.slides.getActiveIndex() < this.slides.length() - 1) {
+      this.nextBtnLabel = "Next"
     }
-    if(this.slides.getActiveIndex()==0){
+    if (this.slides.getActiveIndex() == 0) {
       this.backHidden = true;
     }
     this.slides.lockSwipes(true);
   }
 
-  calculatePrice(){
-    if(this.onlineOrder.numberOfHeads>0&&
-      this.onlineOrder.portraitType!=null &&
-      this.onlineOrder.portraitSize!=null &&
-      this.onlineOrder.matAdd!=null){
-         this.onlineOrder.totalPrice=100;
+  calculatePrice() {
+    if (this.onlineOrder.numberOfHeads > 0 &&
+      this.onlineOrder.portraitType != null &&
+      this.onlineOrder.portraitSize != null &&
+      this.onlineOrder.matAdd != null) {
+      this.onlineOrder.totalPrice = 100;
     }
   }
 
 
-  uploadPhoto(event){
+  uploadPhoto(event) {
 
-     let file = event.target.files[0];
-     let fileName = file.name;
-     let albumPhotosKey = encodeURIComponent(this.onlineOrder.orderId.toString())+ '/';
-     let photoKey = albumPhotosKey + fileName;
+    let file = event.target.files[0];
+    let fileName = file.name;
+    let albumPhotosKey = encodeURIComponent(this.onlineOrder.orderId.toString()) + '/';
+    let photoKey = albumPhotosKey + fileName;
 
-     this.s3.upload({
-       Key: photoKey,
-       Body: file,
-       ACL: 'public-read',
-       Bucket:'portraitist-customer-upload'
-      }, function (err, data) {
-       if (err) {
-         return alert('There was an error uploading your photo: ' + err.message);
-       }
-       alert('Successfully uploaded photo.');
-     })
+    this.s3.upload({
+      Key: photoKey,
+      Body: file,
+      ACL: 'public-read',
+      Bucket: 'portraitist-customer-upload'
+    }, function (err, data) {
+      if (err) {
+        return alert('There was an error uploading your photo: ' + err.message);
+      }
+      alert('Successfully uploaded photo.');
+    })
   }
 }
